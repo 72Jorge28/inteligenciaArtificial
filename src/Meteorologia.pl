a25dc4795c82e544d913dc2c4ac1ca04632af4e4
72Jorge28 <111816730+72Jorge28@users.noreply.github.com>
2023-09-24 20:31:22 -0400

    Update Meteorologia

--- src/Meteorologia.pl
+++ src/Meteorologia.pl
@@ -4,13 +4,13 @@ vientos_menores_63km/h(X):-nl, write(X), write('existe vientos menores a 63km/h?
 nubosidad(X):-nl, write(X), write('existe nubosidad intensa?[s/n]: '), read(R),R=s.
 precipitaciones(X):-nl, write(X), write('existe precipitaciones intensa?[s/n]: '), read(R),R=s.
 aumento_marea_oleaje(X):-nl, write(X), write('aumento en la marea y en el oleaje en las zonas costeras? [s/n]: '), read(R),R=s.
-vientos_mayores_63km/h(X):-nl, write(X), write('existe vientos mayores a 63km/h?[s/n]: '), read(R),R=s.
-vientos_mayores_116km/h(X):-nl, write(X), write('existe vientos mayores a 116km/h?[s/n]: '), read(R),R=s.
+vientos_mayores_63km(X):-nl, write(X), write('existe vientos mayores a 63km/h?[s/n]: '), read(R),R=s.
+vientos_mayores_116km(X):-nl, write(X), write('existe vientos mayores a 116km/h?[s/n]: '), read(R),R=s.
 
 % las reglas 
 
 depresion_tropical(X):-vientos_menores_63km/h (X),nubosidad(X),precipitaciones(X),aumento_marea_oleaje(X),.
-tormenta tropical(X):-vientos_mayores_63km/h(X),nubosidad(X),precipitaciones(X),aumento_marea_oleaje(X),.
+tormenta_tropical(X):-vientos_mayores_63km/h(X),nubosidad(X),precipitaciones(X),aumento_marea_oleaje(X),.
 huracan(X):-vientos_vientos_mayores_116km/h(X),nubosidad(X),precipitaciones(X),aumento_marea_oleaje(X),.
 
 % Pronostico
