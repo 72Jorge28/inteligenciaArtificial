6914d61effc20091859ca0d535524c2c219d9fde
72Jorge28 <jorgeramos7228@gmail.com>
2023-09-24 21:00:01 -0400

    Covid19

---
+++ Covid.pl
@@ -0,0 +1,23 @@
+% Proposiciones
+
+prop(1,fiebreEscalofrios,p)
+prop(2,tos,p)
+prop(3,dificultadRespirar,p)
+prop(4,fatiga,p)
+prop(5,dolorMuscular,p)
+prop(6,dolorCabeza,p)
+prop(7,perdidaOlfato,p)
+prop(8,dolorGarganta,p)
+prop(9,congestionMoqueo,p)
+prop(10,nauseasVomitos,p)
+prop(11,Diarrea,p)
+
+% Preposicion inferible
+
+prep(100,'Covid-19',i)
+
+clase(2,objetivo, [100])
+
+% Las reglas
+
+regla(1,y,[1,2,3,4,5,6,7,8,9,10,11],100,0.7)
