97791b50ef2f9a89db89c718b1ff4a750a8bc4b6
72Jorge28 <jorgeramos7228@gmail.com>
2023-09-24 20:40:21 -0400

    Meteorologia

---
+++ Meteorologia.pl
@@ -0,0 +1,22 @@
+% Proposiciones
+
+prop(1,menor63,p)
+prop(2,nubosidad,p)
+prop(3,presipitaciones,p)
+prop(4,aumentoMarea,p)
+prop(5,mayores63,p)
+prop(6,mayores116,p)
+
+% Preposicion inferible
+
+prep(100,'depresionTropical',i)
+prep(101,'tormentaTropical',i)
+prep(102,'huracan',i)
+
+clase(2,objetivo, [100,101,102])
+
+% Las reglas
+
+regla(1,y,[1,2,3,4],100,0.7)
+regla(1,y,[5,2,3,4],101,0.6)
+regla(1,y,[6,2,3,4],102,0.9)
